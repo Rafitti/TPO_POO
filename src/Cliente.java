3d8d4be8f02c70fc3d26d8d52b722ffa9f272e70
rafaelmiranda93 <ramiranda1993@outlook.com>
2025-10-17 19:08:14 -0300

    quitar clase añadida por error

--- src/Cliente.java
+++ src/Cliente.java
@@ -14,20 +14,4 @@ public class Cliente extends Usuario {
     public void mostrarInfo() {
         System.out.println("Cliente: " + nombre + " | DNI: " + dni);
     }
-}public class Cliente extends Usuario {
-    private String dni;
-
-    public Cliente(String nombre, String dni) {
-        super(nombre);
-        this.dni = dni;
-    }
-
-    public String getDni() {
-        return dni;
-    }
-
-    @Override
-    public void mostrarInfo() {
-        System.out.println("Cliente: " + nombre + " | DNI: " + dni);
-    }
 }
